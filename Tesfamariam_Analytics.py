db8b1e844871f58f3703edb49e3cba41f62a34d2
Tesfamariam <s568695@nwmissouri.edu>
2024-01-28 15:44:55 -0500

    Added conditional execution block for main function

--- Tesfamariam_Analytics.py
+++ Tesfamariam_Analytics.py
@@ -178,3 +178,7 @@ if not os.path.exists(directory_path):
     print(f"Directory '{directory_path}' created successfully")
 else:
     print(f"Directory '{directory_path}' already exists")
+
+# conditional execution
+if __name__ == "__main__":
+    main()
